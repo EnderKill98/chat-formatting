a423ceef4220ad92f3e83c4c8db95565b0b2185e
EnderKill98 <no@mail.tld>
2024-09-01 01:56:31 +0200

    Update comment

--- src/chat.rs
+++ src/chat.rs
@@ -198,8 +198,8 @@ pub enum TextContent {
         score: Score,
     },
     Literal {
-        // For some stupid reason, paper (since 1.21.1) and some plugins generate a object like this: { "": "\n" } (mainly EssentialsX afk message and /version)
-        // This is completely insane, no idea how they got to make that! Accepting "" as "text" should parse it correctly
+        /// Note: For some stupid reason, paper (since 1.21.1) and some plugins generate a object like this: { "": "\n" } (mainly EssentialsX afk message and /version)
+        /// This is completely insane, no idea how they got to make that! Accepting "" as "text" should just parse it correctly.
         #[serde(alias = "")]
         text: String,
     },
